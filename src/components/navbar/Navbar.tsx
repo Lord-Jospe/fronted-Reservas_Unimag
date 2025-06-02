8d8e6310a06dcdc475cb651bcb0867a81c6dc9c8
Lord-Jospe <jdferrer@unimagdalena.edu.co>
2025-06-01 21:44:26 -0500

    Listar de todos los componenetes en adminDasboard

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,27 @@
-import "./navbar.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
-
+import "./navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "../../auth/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const { role } = useAuth(); // Accede al rol del usuario
+  const navigate = useNavigate();
+
+  const handleTitleClick = () => {
+    if (role === "ADMINISTRADOR") {
+      navigate("/admin/dashboard");
+    } else {
+      navigate("/home");
+    }
+  };
   return (
     <header className="custom-navbar d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
       <a
-        href="/home"
         className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
+        style={{ cursor: "pointer" }}
+        onClick={handleTitleClick}
       >
         <svg
           className="bi me-2"
@@ -20,12 +32,18 @@ function Navbar() {
         <span className="fs-4">Reservas Unimagdalena</span>
       </a>
       <ul className="nav nav-pills">
-        {/* Añadir el hola "nombre de estudiante"*/}
-        <li className="nav-item">
-          <a href="/student-panel" className="nav-link active" aria-current="page">
-            <FontAwesomeIcon icon={faBell} />
-          </a>
-        </li>
+        {/* Solo se muestra si el usuario es ESTUDIANTE */}
+        {role === "ESTUDIANTE" && (
+          <li className="nav-item">
+            <a
+              href="/student-panel"
+              className="nav-link active"
+              aria-current="page"
+            >
+              <FontAwesomeIcon icon={faBell} />
+            </a>
+          </li>
+        )}
         <li className="nav-item">
           <a href="/profile" className="nav-link active" aria-current="page">
             <FontAwesomeIcon icon={faUser} />
--- src/pages/adminDashboard/AdminDashboard.tsx
+++ src/pages/adminDashboard/AdminDashboard.tsx
@@ -12,6 +12,15 @@ import EspacioService, {
 import TableData from "../../components/tableData/TableData";
 import { useAuth } from "../../auth/AuthProvider";
 import { jwtDecode } from "jwt-decode";
+import ProblemaAdminService, {
+  ProblemaDtoResponse,
+} from "../../services/ProblemaAdminService";
+import HistorialReservaService, {
+  HistorialReservaDtoResponse,
+} from "../../services/HistorialReservaService";
+import EstudianteService, {
+  EstudianteDTOResponse,
+} from "../../services/EstudianteService";
 
 function AdminDashboard() {
   const [selectedCategory, setSelectedCategory] = useState("Inicio");
@@ -19,16 +28,23 @@ function AdminDashboard() {
 
   const [reservas, setReservas] = useState<ReservaDtoResponse[]>([]);
   const [espacios, setEspacios] = useState<EspacioDTOResponse[]>([]);
+  const [estudiantes, setEstudiantes] = useState<EstudianteDTOResponse[]>([]);
+  const [historialReservas, setHistorialReservas] = useState<
+    HistorialReservaDtoResponse[]
+  >([]);
+  const [problemas, setProblemas] = useState<ProblemaDtoResponse[]>([]);
+
   const [correoUsuario, setCorreoUsuario] = useState<string>("");
 
   const options = [
     "Inicio",
     "Espacios",
     "Reservas",
+    "Estudiantes",
+    "Historial de Reservas",
     "Reporte de problemas",
     "Notificaciones",
     "Configuración",
-    "Usuarios",
   ];
 
   //Obtener el usuario autenticado
@@ -37,7 +53,7 @@ function AdminDashboard() {
     if (token) {
       try {
         const decoded: any = jwtDecode(token);
-        setCorreoUsuario(decoded.correo || decoded.sub); 
+        setCorreoUsuario(decoded.correo || decoded.sub);
       } catch (error) {
         console.error("Error al decodificar token:", error);
       }
@@ -47,20 +63,47 @@ function AdminDashboard() {
   useEffect(() => {
     if (!isAuthenticated || role !== "ADMINISTRADOR") return;
 
-    if (selectedCategory === "Reservas") {
-      ReservasService.getTodasReservas()
-        .then((response) => {
-          setReservas(response.data);
-        })
-        .catch((error) => console.error("Error al obtener reservas:", error));
-    }
+    switch (selectedCategory) {
+      case "Reservas":
+        ReservasService.getTodasReservas()
+          .then((response) => setReservas(response.data))
+          .catch((error) => console.error("Error al obtener reservas:", error));
+        break;
 
-    if (selectedCategory === "Espacios") {
-      EspacioService.listarEspacios()
-        .then((response) => {
-          setEspacios(response.data);
-        })
-        .catch((error) => console.error("Error al obtener espacios:", error));
+      case "Espacios":
+        EspacioService.listarEspacios()
+          .then((response) => setEspacios(response.data))
+          .catch((error) => console.error("Error al obtener espacios:", error));
+        break;
+
+      case "Estudiantes":
+        EstudianteService.findAllEstudiantes()
+          .then((response) => setEstudiantes(response.data))
+          .catch((error) =>
+            console.error("Error al obtener estudiantes:", error)
+          );
+        break;
+
+      case "Historial de Reservas":
+        HistorialReservaService.findAllHistoriales()
+          .then((response) => setHistorialReservas(response.data))
+          .catch((error) =>
+            console.error("Error al obtener historial de reservas:", error)
+          );
+          console.log("Historial de reservas:", historialReservas);
+        break;
+
+      case "Reporte de problemas":
+        ProblemaAdminService.getAllProblemas()
+          .then((response) => setProblemas(response.data))
+          .catch((error) =>
+            console.error("Error al obtener problemas:", error)
+          );
+          console.log("Problemas:", problemas);
+        break;
+
+      default:
+        break;
     }
   }, [selectedCategory, isAuthenticated, role]);
 
@@ -113,6 +156,47 @@ function AdminDashboard() {
                 console.log("Editar problema:", item);
                 // Aquí puedes implementar la lógica para editar el problema
               }}
+            />
+          )}
+
+          {selectedCategory === "Estudiantes" && (
+            <TableData
+              columnas={[
+                { label: "ID", field: "idEstudiante" },
+                { label: "Nombre", field: "nombre" },
+              ]}
+              datos={estudiantes}
+              
+              onEditClick={(item) => console.log("Editar estudiante:", item)}
+            />
+          )}
+
+          {selectedCategory === "Historial de Reservas" && (
+            <TableData
+              columnas={[
+                { label: "ID", field: "idHistorial" },
+                { label: "Fecha", field: "fechaCambio" },
+                { label: "idReserva", field: "idReserva" },
+                { label: "Estado", field: "estadoReserva" },
+                { label: "Comentario", field: "comentario" },
+              ]}
+              datos={historialReservas}
+              onEditClick={(item) => console.log("Editar historial:", item)}
+            />
+          )}
+
+          {selectedCategory === "Reporte de problemas" && (
+            <TableData
+              columnas={[
+                { label: "ID", field: "idProblema" },
+                { label: "Descripción", field: "descripcion" },
+                { label: "Espacio", field: "espacioId" },
+                { label: "Estado", field: "estado" },
+                { label: "Fecha Reporte", field: "fecha" },
+                { label: "Estudiante", field: "idEstudiante" },
+              ]}
+              datos={problemas}
+              onEditClick={(item) => console.log("Editar problema:", item)}
             />
           )}
         </div>
